5aad1655f6c3d4832372a6ba53cdc988198ad5f0
Wispmother Draugr scourge <widraugr@at-o9.21-school.ru>
2020-02-26 15:24:20 +0300

    final

--- file_s/neg_indirect.s
+++
@@ -1,5 +0,0 @@
-.comment "I'M ]ALIIIIVE"
-.name "zork"
-;asdfasdf
-ld 1, r9  ;asdfasdfasdf
-#asdfk
--- sh_md5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./asm $1.s #> /dev/null
-md5 $1.cor
-rm $1.cor
-./vm_champs/asm $1.s # > /dev/null
-md5 $1.cor
-rm $1.cor
